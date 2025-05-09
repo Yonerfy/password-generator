c90559690600b96e49b1e961013e20ce0d3a5ea8
Yonerfy <yonerfy15@gmail.com>
2025-05-09 14:17:47 +0200

    feat: implement password generator with validation for length and selected options
    fix: resolve issues with checkbox reactivity, slider value binding, and button disable state
    
    feat: add dynamic password strength indicator based on selected options
    
    fix: ensure proper image rendering for password strength component
    
    refactor: improve validation logic for password length and checkbox selection

--- src/components/ButtonG.vue
+++ src/components/ButtonG.vue
@@ -4,13 +4,29 @@ const emit = defineEmits(['generate']);
 function handleClick() {
     emit('generate');
 }
+defineProps({
+    isButtonDisabled: {
+        type: Boolean,
+        default: false
+    }
+});
 </script>
 
 <template>
     <div class="button-container flex items-center justify-center relative">
-        <button class="bg-[#A4FFAF] w-full p-[1em] text-[#24232C] cursor-pointer" @click="handleClick">GENERATE</button>
+        <button 
+            class="bg-[#A4FFAF] w-full p-[1em] text-[#24232C] cursor-pointer" :class="{'disabled-btn': isButtonDisabled}"
+            @click="handleClick"
+            >
+            GENERATE
+        </button>
         <img src="../assets/bx_arrow-to-left.svg" alt="generate icon" class="absolute right-[9em] top-1/2 transform -translate-y-1/2 cursor-pointer">
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.disabled-btn {
+  background-color: #a4ffaf3d;
+  cursor: not-allowed; /* Optional: Change cursor to indicate disabled state */
+}
+</style>
