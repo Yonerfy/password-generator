245693da3b87c1b0343aa65d918f1a3156ab4741
Yonerfy <yonerfy15@gmail.com>
2025-05-15 15:48:46 +0200

    fix: prevent button layout shift on hover by keeping border width constant

--- src/components/ButtonG.vue
+++ src/components/ButtonG.vue
@@ -13,31 +13,27 @@ defineProps({
 </script>
 
 <template>
-    <div class="button-container flex items-center justify-center relative hover:bg-[#24232c] hover:border-3 hover:border-[#A4FFAF] hover:text-[#A4FFAF]">
+    <div class="button-container flex items-center justify-center relative">
         <button 
-            class="bg-[#A4FFAF] hover:bg-[#24232c] w-full p-[1em] text-[#24232C] cursor-pointer hover:text-[#A4FFAF]" :class="{'disabled-btn': isButtonDisabled}"
+            class="w-full p-[1em] text-[#24232C] bg-[#A4FFAF] border-2 border-[#a4ffaf3d] transition-all duration-300 cursor-pointer
+                   hover:bg-[#24232c] hover:text-[#A4FFAF] hover:border-[#A4FFAF]"
+            :class="{'disabled-btn': isButtonDisabled}"
+            :disabled="isButtonDisabled"
             @click="handleClick"
-            >
+        >
             GENERATE
         </button>
-        <img  src="../assets/bx_arrow-to-left.svg" alt="generate icon" class="btn-arrow absolute right-[9em] top-1/2 transform -translate-y-1/2 cursor-pointer">
+        <img src="../assets/bx_arrow-to-left.svg" alt="generate icon" class="btn-arrow absolute right-[9em] top-1/2 transform -translate-y-1/2 cursor-pointer">
     </div>
 </template>
 
 <style scoped>
+.disabled-btn {
+    background-color: #a4ffaf3d !important;
+    color: #24232C !important;
+    border-color: #a4ffaf00 !important;
+    cursor: not-allowed !important;
 
-/* .button-container {
-    border: 1px solid #A4FFAF;
-} */
-.button-container{
-    border: 1px solid #a4ffaf3d;
 }
-.disabled-btn, .disabled-btn:hover,.button-container:hover {
-    background-color: #a4ffaf3d;
-    cursor: not-allowed; /* Optional: Change cursor to indicate disabled state */
-    color: #24232C;
-    border-color: #a4ffaf3d;
-}
-
-
 </style>
+
